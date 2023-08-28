8123b3b7e8da234bb581fbe236f38d45e8e9440a
moon-zhou <ayimin1989@163.com>
2023-08-28 17:20:18 +0800

    feat: AtomicInteger

---
+++ java-common/src/main/java/org/moonzhou/atomic/Demo001AtomicInteger.java
@@ -0,0 +1,21 @@
+package org.moonzhou.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author moon zhou
+ * @version 1.0
+ * @description:
+ * @date 2023/8/28 16:42
+ */
+public class Demo001AtomicInteger {
+    public static void main(String[] args) {
+        AtomicInteger order = new AtomicInteger(0);
+        System.out.println(order.getAndIncrement()); // 0
+        System.out.println(order); // 1
+
+        AtomicInteger order2 = new AtomicInteger(0);
+        System.out.println(order2.incrementAndGet()); // 1
+        System.out.println(order2); // 1
+    }
+}
