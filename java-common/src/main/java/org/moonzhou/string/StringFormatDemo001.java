e46574c430d5902d44f41e953d68737a1d1be11a
moon-zhou <ayimin1989@163.com>
2023-10-09 11:13:29 +0800

    feat(String): String.format

---
+++ java-common/src/main/java/org/moonzhou/string/StringFormatDemo001.java
@@ -0,0 +1,19 @@
+package org.moonzhou.string;
+
+/**
+ * @author moon zhou
+ * @version 1.0
+ * @description:
+ * @date 2023/10/9 11:02
+ */
+public class StringFormatDemo001 {
+    public static void main(String[] args) {
+        String format = "str result: %s";
+
+        String stringResult = String.format(format, "this is a string.");
+        System.out.println(stringResult);
+
+        String longResult = String.format(format, 999L);
+        System.out.println(longResult);
+    }
+}
