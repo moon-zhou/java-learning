e29e9cb8a4817d73ddeb026016b8c05c8522c252
moon-zhou <ayimin1989@163.com>
2025-07-01 23:16:11 +0800

    feat(reference): optimize strong reference demo

--- java-common/src/main/java/org/moonzhou/reference/strong/Demo001.java
+++ java-common/src/main/java/org/moonzhou/reference/strong/Demo001.java
@@ -1,5 +1,7 @@
 package org.moonzhou.reference.strong;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author moon zhou
  * @description
@@ -9,17 +11,36 @@ package org.moonzhou.reference.strong;
 public class Demo001 {
     public static void main(String[] args) {
         MyObject obj = new MyObject(); // 强引用
+        System.out.println("未置空，Gc前：" + obj);
+
         System.gc(); // 触发GC
-        System.out.println("GC后对象状态：" + obj); // 对象依然存在
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("未置空，GC后对象状态：" + obj); // 对象依然存在
 
         obj = null; // 断开强引用
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.gc();
-        System.out.println("置空后GC结果：" + obj); // 对象被回收
+        System.out.println("置空后，GC结果：" + obj); // 对象被回收
     }
 
     static class MyObject {
         public MyObject() {
 
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("对象被回收了, finalize()");
         }
     }
 }
